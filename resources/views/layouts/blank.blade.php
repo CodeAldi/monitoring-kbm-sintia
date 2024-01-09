e5877a88921e765fe3fc112b8062aa9e11d14fa4
aldi <codealdi@gmail.com>
2024-01-09 13:22:16 +0700

    ubah title

--- resources/views/layouts/blank.blade.php
+++ resources/views/layouts/blank.blade.php
@@ -27,7 +27,7 @@
       content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0"
     />
 
-    <title>{{ $title }}</title>
+    <title>{{ $title }} | {{ config('app.name') }}</title>
 
     <meta name="description" content="" />
 
