18948c22e0e0599ebce616da8a16b296e50b4de5
aldi <codealdi@gmail.com>
2024-01-22 23:08:20 +0700

    add constrained on update & on delete casacade

--- database/migrations/2024_01_18_172049_create_guru_mapels_table.php
+++ database/migrations/2024_01_18_172049_create_guru_mapels_table.php
@@ -13,9 +13,9 @@ return new class extends Migration
     {
         Schema::create('guru_mapel', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('users_id');
-            $table->foreignId('mapel_id');
-            $table->foreignId('kelas_id');
+            $table->foreignId('users_id')->constrained()->onUpdate('cascade')->onDelete('cascade');
+            $table->foreignId('mapel_id')->constrained('mapel')->onUpdate('cascade')->onDelete('cascade');
+            $table->foreignId('kelas_id')->constrained('kelas')->onUpdate('cascade')->onDelete('cascade');
             $table->string('semester')->nullable();
             $table->year('tahun')->nullable();
             $table->timestamps();
