ac240a2a9de25fc5cdf8d00a930cc88531a4e97d
aldi <codealdi@gmail.com>
2024-02-05 14:23:33 +0700

    store fixing

--- app/Http/Controllers/JadwalMengajarController.php
+++ app/Http/Controllers/JadwalMengajarController.php
@@ -6,6 +6,8 @@ use App\Models\EventJadwalMengajar;
 use App\Models\GuruMapel;
 use App\Models\JadwalMengajar;
 use App\Models\Kelas;
+use DateInterval;
+use DatePeriod;
 use Illuminate\Http\Request;
 
 class JadwalMengajarController extends Controller
@@ -51,23 +53,31 @@ class JadwalMengajarController extends Controller
      */
     public function store(Request $request)
     {
-        $gurudanmapel = GuruMapel::find($request->guru_mapel_id);
-        $title = $gurudanmapel->mapel->nama_mapel;
-        $start = $request->tanggal_awal . 'T' . $request->jam_mulai;
-        $end = $request->tanggal_awal . 'T' . $request->jam_selesai;
-        $event = new EventJadwalMengajar();
-        $event->title = $title;
-        $event->start = $start;
-        $event->end   = $end;
-        $event->save();
-
-        $jadwalMengajar = new JadwalMengajar();
-        $jadwalMengajar->event_jadwal_mengajar_id = $event->id;
-        $jadwalMengajar->guru_mapel_id = $gurudanmapel->id;
-        $jadwalMengajar->kelas_id = $gurudanmapel->kelas_id;
-        $jadwalMengajar->tanggal_mulai = $request->tanggal_awal;
-        $jadwalMengajar->tanggal_selesai = $request->tanggal_selesai;
-        $jadwalMengajar->save();
+        $interval = new DateInterval('P1W');
+        $start_date = date_create($request->tanggal_awal);
+        $end_date = date_create($request->tanggal_selesai);
+        $date_range = new DatePeriod($start_date, $interval, $end_date);
+        // dd($date->format('Y-m-d'));
+        foreach ($date_range as $date) {
+            $gurudanmapel = GuruMapel::find($request->guru_mapel_id);
+            $title = $gurudanmapel->mapel->nama_mapel;
+            $start = $date->format('Y-m-d') . 'T' . $request->jam_mulai;
+            $end = $date->format('Y-m-d') . 'T' . $request->jam_selesai;
+            $event = new EventJadwalMengajar();
+            $event->title = $title;
+            $event->start = $start;
+            $event->end   = $end;
+            $event->save();
+    
+            $jadwalMengajar = new JadwalMengajar();
+            $jadwalMengajar->event_jadwal_mengajar_id = $event->id;
+            $jadwalMengajar->guru_mapel_id = $gurudanmapel->id;
+            $jadwalMengajar->kelas_id = $gurudanmapel->kelas_id;
+            $jadwalMengajar->tanggal_mulai = $request->tanggal_awal;
+            $jadwalMengajar->tanggal_selesai = $request->tanggal_selesai;
+            $jadwalMengajar->save();
+            
+        }
         return back();
     }
 
