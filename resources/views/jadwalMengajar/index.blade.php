db2870a53b064bb68bd2eb1e867dc5b5e38f78ea
aldi <codealdi@gmail.com>
2024-02-21 00:42:58 +0700

    hide saturday

--- resources/views/jadwalMengajar/index.blade.php
+++ resources/views/jadwalMengajar/index.blade.php
@@ -26,12 +26,12 @@
                 },
                 businessHours: {
                 // days of week. an array of zero-based day of week integers (0=Sunday)
-                daysOfWeek: [ 1, 2, 3, 4, 5, 6 ], // Monday - Thursday
+                daysOfWeek: [ 1, 2, 3, 4, 5 ], // Monday - Thursday
                 
                 startTime: '07:30', // a start time (10am in this example)
                 endTime: '16:00', // an end time (6pm in this example)
                 },
-                hiddenDays: [0],
+                hiddenDays: [0,6],
                 events: dataEvent,
                 allDaySlot: false,
                 eventClick: function(info) {
