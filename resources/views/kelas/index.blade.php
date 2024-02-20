835d1db7968f90e7720176418b6570096e60eb28
aldi <codealdi@gmail.com>
2024-02-20 17:53:20 +0700

    minor changes on tingkat kelas

--- resources/views/kelas/index.blade.php
+++ resources/views/kelas/index.blade.php
@@ -77,7 +77,7 @@
                         <select name="tingkat_kelas" id="tingkat_kelas" class="form-select">
                             <option>-- select --</option>
                             <option value="10">X / 10</option>
-                            <option value="11">XII / 11</option>
+                            <option value="11">XI / 11</option>
                             <option value="12">X / 12</option>
                         </select>
                     </div>
