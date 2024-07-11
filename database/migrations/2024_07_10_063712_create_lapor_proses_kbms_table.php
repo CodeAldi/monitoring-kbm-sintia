b3fd2329e07b724efd49cf399295b014b224381e
aldi-computer-home <codealdi@gmail.com>
2024-07-12 05:04:38 +0700

    sementara

---
+++ database/migrations/2024_07_10_063712_create_lapor_proses_kbms_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('lapor_proses_kbms', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('lapor_proses_kbms');
+    }
+};
